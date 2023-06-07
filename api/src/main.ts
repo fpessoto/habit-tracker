1a8916011bb77cddfc145b2746ab43d9ce56aff3
fpessoto <felipe.pessoto@hotmail.com>
2023-06-07 11:34:56 -0300

    adding interceptors

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,13 +6,16 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './infrastructure/common/filter/exception.filter';
 import { LoggerService } from './infrastructure/logger/logger.service';
+import { LoggingInterceptor } from './infrastructure/common/interceptors/logger.interceptor';
+import { ResponseInterceptor } from './infrastructure/common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
   app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   const config = new DocumentBuilder()
     .setTitle('Median')
